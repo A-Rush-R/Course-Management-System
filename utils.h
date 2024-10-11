7b18819ff6e36e45822da8773a4d927740b6083e
A-Rush-R <arush.up@gmail.com>
2024-10-11 13:06:48 +0530

    remove comments

--- utils.h
+++ utils.h
@@ -8,5 +8,5 @@ void checkGreaterThanZero(double value, const std::string& name);
 void checkNotEmpty(const std::string str, const std::string& name);
 void checkGreaterThan(int big,int small, const std::string& bigName, const std::string& smallName);
 
-#endif // UTILS_H
+#endif
 
